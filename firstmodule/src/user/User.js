111de1c9267a980a18310ce9d8efd081d834f4ed
omid najaee <o.najaee@gmail.com>
2017-06-14 10:13:02 +0200

    added follow() and unfollow() functionality

--- firstmodule/src/user/User.js
+++ firstmodule/src/user/User.js
@@ -4,26 +4,43 @@ const is = validatorJS.Assert;
 const validator = validatorJS.validator();
 
 module.exports = class User {
-  constructor(firstName, lastName, email) {
-    this._firstName = firstName;
-    this._lastName = lastName;
-    this._email = email;
-  }
+	constructor(firstName, lastName, email) {
+		this._firstName = firstName;
+		this._lastName = lastName;
+		this._email = email;
+		this._followers = [];
+	}
 
-  toString() {
-    return `First name: ${this._firstName}, last name: ${this._lastName}, email: ${this.email}`;
-  }
+	toString() {
+		return `First name: ${this._firstName}, last name: ${this._lastName}, email: ${this.email}`;
+	}
 
-  set email(email) {
-    let valid = validator.validate(email, [ is.email() ] );
-    if(typeof valid === 'boolean' && valid) {
-      this._email = email;
-    } else {
-      throw new Error('Invalid email adress');
-    }
-  }
+	set email(email) {
+		let valid = validator.validate(email, [ is.email() ] );
+		if(typeof valid === 'boolean' && valid) {
+			this._email = email;
+		} else {
+			throw new Error('Invalid email adress');
+		}
+	}
 
-  get email() {
-    return this._email;
-  }
+	get email() {
+		return this._email;
+	}
+
+	get followers() {
+		return this._followers;
+	}
+
+	follow(user) {
+		user.followers.push(this);
+	}
+
+	unfollow(user) {
+		let index = user._followers.findIndex(x => x.email === this.email);
+		
+		if (index > -1 )
+			user._followers.splice(index, 1);
+	}
+
 }
