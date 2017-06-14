7f58389f09ba61f384d0b4478067e1ac0596853a
omid najaee <o.najaee@gmail.com>
2017-06-14 09:53:07 +0200

    rename admin.js to Admin.js

--- firstmodule/src/admin/Admin.js
+++ firstmodule/src/admin/Admin.js
@@ -1,13 +1,9 @@
-'use strict';
-
 const User = require('../user/User');
-const _ = require('lodash');
 
 module.exports = class Admin extends User {
 	constructor(firstName, lastName, email, secretPassword) {
 		super(firstName, lastName, email);
 		this._secretPassword = secretPassword;
-
 	}
 
 	toString() {
